07d778239cb311c7c8322337e6138579d80023d8
wisnuu0 <145241773+wisnuu0@users.noreply.github.com>
2023-09-18 20:14:15 +0700

    <wisnuu0> add power function

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const multiple = (a, b) => a * b;
 const divide = (a, b) => a / b;
 const minus = (a,b) => a - b;
 const plus = (a,b) => a + b;
+const power = (a,b) => a ** b;
 
 console.log(divide(6,2))
 console.log(multiple(2,3))
 console.log(minus(5,3))
 console.log(plus(4,8))
+console.log(power(2,3))
