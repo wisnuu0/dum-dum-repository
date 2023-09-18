c833ed2c54f435ec0c81e716392671e50e14f2ea
Wisnu Andika <wisnuandika104@gmail.com>
2023-09-18 20:27:33 +0700

    <pendragonnn> add greet function

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ const divide = (a, b) => a / b;
 const minus = (a,b) => a - b;
 const plus = (a,b) => a + b;
 const power = (a,b) => a ** b;
+const greet = () => console.log('halo')
 
 console.log(divide(6,2))
 console.log(multiple(2,3))
 console.log(minus(5,3))
 console.log(plus(4,8))
 console.log(power(2,3))
+greet()
