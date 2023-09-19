fc6deabadfd06473955ce40108947382a8495edd
wisnuu0 <145241773+wisnuu0@users.noreply.github.com>
2023-09-19 18:26:22 +0700

    <wisnuu0> add doubled funcrion

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ const minus = (a,b) => a - b;
 const plus = (a,b) => a + b;
 const power = (a,b) => a ** b;
 const greet = () => console.log('halo')
+const doubled = (a) => a 2;
 
 console.log(divide(6,2))
 console.log(multiple(2,3))
 console.log(minus(5,3))
 console.log(plus(4,8))
 console.log(power(2,3))
+console.log(doubled(2))
 greet()
